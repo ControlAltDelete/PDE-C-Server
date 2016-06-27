4f789bd5bca654c1917ec923847655e75c7228ff
inyongthegr8 <inyong42@gmail.com>
2016-06-27 13:16:35 +0800

    Push files
    Can decode save files

--- src/server/Server.java
+++ src/server/Server.java
@@ -7,10 +7,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
+import database.dao.ActivityDAO;
+import database.dao.DeliverableDAO;
 import database.dao.StudentDAO;
+import database.objects.Activity;
+import database.objects.Deliverable;
 import database.objects.Student;
+import service.FileDecoder;
 
 public class Server extends Thread
 {
@@ -35,20 +41,60 @@ public class Server extends Thread
 	    String clientSentence = inFromClient.readLine();
 	    System.out.println("From client: "+clientSentence+"\n");
 	    ArrayList<String> info = new ArrayList<String>();
-	    
-	    while(clientSentence.indexOf(",") != -1)
-	    {
-			String temp = clientSentence.substring(0, clientSentence.indexOf(","));
-			info.add(temp);
-			clientSentence = clientSentence.substring(clientSentence.indexOf(",") + 1);
+	    String type = clientSentence.substring(0, clientSentence.indexOf(","));
+	    clientSentence = clientSentence.substring(clientSentence.indexOf(",") + 1);
+		
+		if(type.equals("activity"))
+		{
+			while(clientSentence.indexOf(",") != -1)
+		    {
+				String temp = clientSentence.substring(0, clientSentence.indexOf(","));
+				info.add(temp);
+				clientSentence = clientSentence.substring(clientSentence.indexOf(",") + 1);
+			}
+		    
+		    info.add(clientSentence);
+		    clientSentence = "";
+		    FileDecoder fd = new FileDecoder();
+		    //fd.convertToFile(info.get(3), info.get(5));
+		    //Activity act = new Activity(Integer.parseInt(info.get(0)), info.get(1), info.get(2), new Timestamp(System.currentTimeMillis()), info.get(4), info.get(5));
+		    ActivityDAO adao = new ActivityDAO();
+		    //adao.addActivity(act);
 		}
-	    
-	    info.add(clientSentence);
-	    clientSentence = "";
-	    
-	    Student stu = new Student(Integer.parseInt(info.get(0)), info.get(1), info.get(2), info.get(3), info.get(4));
-	    StudentDAO sdao = new StudentDAO();
-	    sdao.addStudent(stu);
+		else if(type.equals("deliverable"))
+		{
+			while(clientSentence.indexOf(",") != -1)
+		    {
+				String temp = clientSentence.substring(0, clientSentence.indexOf(","));
+				info.add(temp);
+				clientSentence = clientSentence.substring(clientSentence.indexOf(",") + 1);
+			}
+		    
+		    info.add(clientSentence);
+		    clientSentence = "";
+		    FileDecoder fd = new FileDecoder();
+		    fd.convertToFile(info.get(3), info.get(5));
+		    //Deliverable del = new Deliverable(Integer.parseInt(info.get(0)), Integer.parseInt(info.get(1)), Integer.parseInt(info.get(2)), new File(System.getProperty("user.dir")+"/src/"+info.get(3)), new Timestamp(System.currentTimeMillis()), info.get(5), Float.parseFloat(info.get(6)));
+		    DeliverableDAO ddao = new DeliverableDAO();
+		    //ddao.addDeliverable(del);
+		}
+		else
+		{
+			while(clientSentence.indexOf(",") != -1)
+		    {
+				String temp = clientSentence.substring(0, clientSentence.indexOf(","));
+				info.add(temp);
+				clientSentence = clientSentence.substring(clientSentence.indexOf(",") + 1);
+			}
+		    
+		    info.add(clientSentence);
+		    clientSentence = "";
+		    
+		    Student stu = new Student(Integer.parseInt(info.get(0)), info.get(1), info.get(2), info.get(3), info.get(4));
+		    StudentDAO sdao = new StudentDAO();
+		    sdao.addStudent(stu);
+		}
+		
 		server.close();
 	  }
 	  
@@ -58,6 +104,8 @@ public class Server extends Thread
 	  }
 	}
   }
+  
+  
   
   public static void main(String[] args)
   {

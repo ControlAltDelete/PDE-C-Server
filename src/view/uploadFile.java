19154bcdd3976932e179995c0ab365682f0e9997
monmilo9 <monzonmilo@yahoo.com>
2016-07-31 22:34:09 +0300

    Added Upload File Chooser and controller package for File Loading

--- src/view/uploadFile.java
+++ src/view/uploadFile.java
@@ -1,10 +1,15 @@
 package view;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import controller.fileops.FileLoad;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -16,11 +21,15 @@ import java.io.FileReader;
 import java.net.Socket;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.awt.event.ActionEvent;
 
 public class uploadFile extends JPanel {
 	private JTextField textField;
 	private JTextField textField_1;
+	private FileLoad loader;
+	private FileNameExtensionFilter pdfFilter;
     JFileChooser fc;
     JButton b, b1;
     JTextField tf;
@@ -29,11 +38,16 @@ public class uploadFile extends JPanel {
     DataOutputStream dout;
     DataInputStream din;
     int i;
-    File pdf;
 	/**
 	 * Create the panel.
 	 */
 	public uploadFile() {
+		loader = new FileLoad();
+		fc = new JFileChooser();
+		pdfFilter = new FileNameExtensionFilter(
+					"PDF Documents", "pdf");
+		fc.setFileFilter(pdfFilter);
+
 		setLayout(null);
 		
 		textField = new JTextField();
@@ -46,29 +60,13 @@ public class uploadFile extends JPanel {
 		add(lblNewLabel);
 		
 		JButton btnChooseFile = new JButton("Choose File");
-		btnChooseFile.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-				    try {
-				        if (e.getSource() == b) {
-				            int x = fc.showOpenDialog(null);
-				            if (x == JFileChooser.APPROVE_OPTION) {
-				                pdf = fc.getSelectedFile();
-				                tf.setText(pdf.getAbsolutePath());
-				                b1.setEnabled(true);
-				            } else {
-				                pdf = null;
-				                tf.setText(null);
-				                b1.setEnabled(false);
-				            }
-				        }
-				      //  if (e.getSource() == b1) {
-				          //  send();
-				       // }
-				    } catch (Exception ex) {
-				    }
+		btnChooseFile.addActionListener(new ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e) 
+				{
+				  chooseFile(); // Returns FilePath. Upload not yet implemented
 				}
-		});
-
+			});
 		btnChooseFile.setBounds(129, 81, 116, 23);
 		add(btnChooseFile);
 		
@@ -86,4 +84,28 @@ public class uploadFile extends JPanel {
 		add(btnSend);
 
 	}
+	
+	public Path chooseFile()
+	{
+		int returnVal = fc.showOpenDialog(this);
+		Path filePath = null;
+	
+		if (returnVal == JFileChooser.APPROVE_OPTION)
+		{
+			Path path = Paths.get(fc.getSelectedFile().getAbsolutePath());
+			filePath = path;
+			String ext = path.toString();
+			if (loader.checkerpdf(ext))
+			{
+				//textField.setText(ext); Please Add a Text Field/label where the path can be placed
+			}
+			else
+			{
+				JOptionPane.showMessageDialog(null, "Not a PDF File.", "Error", JOptionPane.ERROR_MESSAGE);
+				filePath = null;
+			}
+		}
+		return filePath;
+	}
+	
 }

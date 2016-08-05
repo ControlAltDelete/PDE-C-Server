341c785dc18be4c6b542b38db634dd001083bd26
Raymund Pua <zebedee_pua@rocketmail.com>
2016-08-05 16:32:46 +0800

    Revert "CSV Unfinished"
    This reverts commit 00ff7d0a38fde3e3fc2e13d3425603713a640f67.

--- src/controller/fileops/FileLoad.java
+++ src/controller/fileops/FileLoad.java
@@ -1,17 +1,13 @@
 package controller.fileops;
 
-import java.awt.List;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import com.opencsv.CSVReader;
 
 public class FileLoad 
 {
@@ -70,23 +66,4 @@ public class FileLoad
 	
 	return matcher.matches();
   }
-  
-  public boolean checkercsv(String fileName)
-  {
-	String codePattern = "([^\\s]+(\\.(?i)(csv))$)";
-	pattern = Pattern.compile(codePattern);
-	matcher = pattern.matcher(fileName);
-	
-	return matcher.matches();
-  }
-  
-  public void readCSV(String pathChosen) throws IOException
-  {  	
-	  
-	CSVReader reader = new CSVReader(new FileReader("C://Users//Raymund Pua//Desktop//1.csv"));
-  	System.out.println("Initiatated readCSV");
-    java.util.List<String[]> myEntries = reader.readAll();
-    System.out.println(myEntries.indexOf(myEntries));
-  }
-  
 }

9e10a9a336d820557e965574184d3f76f6bd6d88
inyongthegr8 <inyong42@gmail.com>
2016-08-05 20:13:42 +0800

    Fixed variables, Added activity deadline and leap.
    Leap year checker included.

--- src/view/uploadFile.java
+++ src/view/uploadFile.java
@@ -4,6 +4,8 @@ import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import controller.fileops.FileLoad;
+import database.dao.ActivityDAO;
+import database.objects.Activity;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -17,17 +19,22 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.Socket;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class uploadFile extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtActName;
+	private JTextField txtActNum;
 	private FileLoad loader;
 	private FileNameExtensionFilter pdfFilter;
     JFileChooser fc;
@@ -38,7 +45,7 @@ public class uploadFile extends JPanel {
     DataOutputStream dout;
     DataInputStream din;
     int i;
-    private JTextField textField_2;
+    private JTextField txtFilePath;
 	/**
 	 * Create the panel.
 	 */
@@ -51,47 +58,101 @@ public class uploadFile extends JPanel {
 
 		setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(129, 50, 176, 20);
-		add(textField);
-		textField.setColumns(10);
+		txtActName = new JTextField();
+		txtActName.setBounds(174, 54, 260, 20);
+		add(txtActName);
+		txtActName.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Activity Name");
-		lblNewLabel.setBounds(39, 53, 80, 14);
+		lblNewLabel.setBounds(39, 57, 125, 14);
 		add(lblNewLabel);
+		
+		txtActNum = new JTextField();
+		txtActNum.setBounds(174, 23, 260, 20);
+		add(txtActNum);
+		txtActNum.setColumns(10);
+		
+		JLabel lblActivityNo = new JLabel("Activity No.");
+		lblActivityNo.setBounds(39, 28, 125, 14);
+		add(lblActivityNo);
+		
+		txtFilePath = new JTextField();
+		txtFilePath.setEditable(false);
+		txtFilePath.setBounds(174, 147, 260, 20);
+		add(txtFilePath);
+		txtFilePath.setColumns(10);
+		
+		JLabel lblPathFile = new JLabel("Path File");
+		lblPathFile.setBounds(39, 150, 125, 14);
+		add(lblPathFile);
+		
+		JLabel lblActivityDeadline = new JLabel("Activity Deadline");
+		lblActivityDeadline.setBounds(39, 88, 125, 14);
+		add(lblActivityDeadline);
+		
+		JComboBox cmbYear = new JComboBox();
+		cmbYear.setModel(new DefaultComboBoxModel(new String[] {"Year", "2016", "2017"}));
+		cmbYear.setBounds(354, 85, 80, 20);
+		add(cmbYear);
+		
+		JComboBox cmbMonth = new JComboBox();
+		cmbMonth.setModel(new DefaultComboBoxModel(new String[] {"Month", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}));
+		cmbMonth.setBounds(174, 85, 80, 20);
+		add(cmbMonth);
+		
+		JComboBox cmbDay = new JComboBox();
+		cmbDay.setModel(new DefaultComboBoxModel(new String[] {"Day", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
+		cmbDay.setBounds(264, 85, 80, 20);
+		add(cmbDay);
+		
+		JComboBox cmbHour = new JComboBox();
+		cmbHour.setModel(new DefaultComboBoxModel(new String[] {"hour", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18"}));
+		cmbHour.setBounds(174, 116, 80, 20);
+		add(cmbHour);
+		
+		JComboBox cmbMinute = new JComboBox();
+		cmbMinute.setModel(new DefaultComboBoxModel(new String[] {"min", "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"}));
+		cmbMinute.setBounds(264, 116, 80, 20);
+		add(cmbMinute);
 		
 		JButton btnChooseFile = new JButton("Choose File");
 		btnChooseFile.addActionListener(new ActionListener() 
 			{
 				public void actionPerformed(ActionEvent e) 
 				{
-				  chooseFile(); // Returns FilePath. Upload not yet implemented
+				  Path p = chooseFile(); // Returns FilePath. Upload not yet implemented
+				  txtFilePath.setText(p.toString());
 				}
 			});
-		btnChooseFile.setBounds(129, 109, 116, 23);
+		btnChooseFile.setBounds(174, 178, 116, 23);
 		add(btnChooseFile);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(129, 23, 176, 20);
-		add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblActivityNo = new JLabel("Activity No.");
-		lblActivityNo.setBounds(39, 28, 80, 14);
-		add(lblActivityNo);
-		
 		JButton btnSend = new JButton("Send");
-		btnSend.setBounds(129, 143, 116, 23);
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ActivityDAO adao = new ActivityDAO();
+				Activity a = new Activity();
+				a.setActivityName(txtActName.getText());
+				a.setActivityID(Integer.parseInt(txtActNum.getText()));
+				a.setActivityTimeStamp(new Timestamp(System.currentTimeMillis()));
+				a.setActivityFile(new File(txtFilePath.getName()));
+				a.setActivityFilename(a.getActivityFile().getName());
+				try
+				{
+					adao.addActivity(a);
+				}
+				catch (FileNotFoundException fnfe)
+				{
+					System.out.println("File not found. It may have been deleted during the process.");
+				}
+				catch (SQLException e) 
+				{
+					System.out.println("No SQL Connection");
+				}
+			}
+		});
+		btnSend.setBounds(318, 178, 116, 23);
 		add(btnSend);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(129, 78, 176, 20);
-		add(textField_2);
-		textField_2.setColumns(10);
-		
-		JLabel lblPathFile = new JLabel("Path file");
-		lblPathFile.setBounds(39, 78, 65, 14);
-		add(lblPathFile);
 
 	}
 	
@@ -107,7 +168,7 @@ public class uploadFile extends JPanel {
 			String ext = path.toString();
 			if (loader.checkerpdf(ext))
 			{
-				  textField_2.setText(ext);
+				  txtFilePath.setText(ext);
 			}
 			else
 			{
@@ -116,5 +177,51 @@ public class uploadFile extends JPanel {
 			}
 		}
 		return filePath;
+	}
+	
+	public DefaultComboBoxModel<String> monthDayYearChecker(String month, String year)
+	{
+		DefaultComboBoxModel<String> res = new DefaultComboBoxModel<String>();
+		boolean leap = false;
+		if((month.equals("Month") && year.equals("Year")) || month.equals("Month") || year.equals("Year"))
+		{
+			// do nothing
+		}
+		else
+		{
+			int y = Integer.parseInt(year);
+			leap = ((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0);
+			if(leap)
+			{
+				if(month.equals("Jan") || month.equals("Mar") || month.equals("May") || month.equals("Jul") || month.equals("Aug") || month.equals("Oct") || month.equals("Dec"))
+				{
+					res = new DefaultComboBoxModel(new String[] {"Day", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"});
+				}
+				else if(month.equals("Apr") || month.equals("Jun") || month.equals("Sep") || month.equals("Nov"))
+				{
+					res = new DefaultComboBoxModel(new String[] {"Day", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"});
+				}
+				else if(month.equals("Feb"))
+				{
+					res = new DefaultComboBoxModel(new String[] {"Day", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29"});
+				}
+			}
+			else
+			{
+				if(month.equals("Jan") || month.equals("Mar") || month.equals("May") || month.equals("Jul") || month.equals("Aug") || month.equals("Oct") || month.equals("Dec"))
+				{
+					res = new DefaultComboBoxModel(new String[] {"Day", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"});
+				}
+				else if(month.equals("Apr") || month.equals("Jun") || month.equals("Sep") || month.equals("Nov"))
+				{
+					res = new DefaultComboBoxModel(new String[] {"Day", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"});
+				}
+				else if(month.equals("Feb"))
+				{
+					res = new DefaultComboBoxModel(new String[] {"Day", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28"});
+				}
+			}
+		}
+		return res;
 	}
 }

e60474da633aa342c601680b22da6043c5048479
inyongthegr8 <inyong42@gmail.com>
2016-11-14 10:13:02 +0800

    Added count statements
    To prevent getting via manual array list which can result to more
    operations

--- src/database/dao/DeliverableDAO.java
+++ src/database/dao/DeliverableDAO.java
@@ -245,6 +245,18 @@ public class DeliverableDAO extends DAO{
         return deliverables;
     }
     
+    public int getDeliverableCountByStudent(int sID) throws SQLException, IOException{
+    	int result = 0;
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select count(distinct ActivityID) as ActivitiesObtained from Deliverable where StudentID = ? order by DeliverableID");
+        preparedStatement.setInt(1, sID);
+        ResultSet resultSet = query(preparedStatement);
+        while (resultSet.next()) {
+        	result = resultSet.getInt("ActivitiesObtained");
+        }
+        return result;
+    }
+    
     public boolean isLate(int studentID, int activityID)throws SQLException, IOException
     {
     	Timestamp deliverableSubmitted = new Timestamp(System.currentTimeMillis()); // default

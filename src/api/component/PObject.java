9f0306d4c354c4a7fb4db39df6433a3caaf7b27e
inyongthegr8 <inyong42@gmail.com>
2016-12-14 14:18:44 +0800

    PObject -@ControlAltDelete

--- src/api/component/PObject.java
+++ src/api/component/PObject.java
@@ -2,6 +2,11 @@ package api.component;
 
 import java.io.Serializable;
 
+/**
+ * Used for receiving an <code>Object</code> from PDE-C Client.
+ * @author Alexander John Jose
+ *
+ */
 public class PObject extends Object implements Serializable
 {
 
